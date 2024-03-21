535562129744440f2f3ffb1f357ca5de51f74344
Aparkin Matvey <152659509+impartac@users.noreply.github.com>
2024-03-21 20:34:03 +0300

    Create FileLogger.cs

---
+++ Libruary/FileLogger.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libruary
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string _filePath;
+
+        public FileLogger(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            string message = formatter(state, exception);
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(_filePath, true))
+                {
+                    writer.WriteLine($"{DateTime.Now}: [{logLevel}] - {message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while logging: {ex.Message}");
+            }
+        }
+    }
+}
