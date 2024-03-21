3ff67938bca3e2c8185b80ed03362a8648f73cbe
Aparkin Matvey <152659509+impartac@users.noreply.github.com>
2024-03-21 21:04:00 +0300

    Create LoggerFactoryExtensions.cs

---
+++ Libruary/LoggerFactoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Libruary
+{
+    public static class LoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory loggerFactory, string filePath)
+        {
+            loggerFactory.AddProvider(new FileLoggerProvider(filePath));
+            return loggerFactory;
+        }
+    }
+
+}
