f3adbf5a74c2439271eb44af40184cc4c9dc4eeb
Aparkin Matvey <152659509+impartac@users.noreply.github.com>
2024-03-21 20:34:05 +0300

    Create FileLoggerProvider.cs

---
+++ Libruary/FileLoggerProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libruary
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private readonly string _filePath;
+
+        public FileLoggerProvider(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(_filePath);
+        }
+    }
+}
